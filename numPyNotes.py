0b3e45ee169758e1d6cd82c9d20437b0c2c27f60
kathrynspeltz <kathrynspeltz@gmail.com>
2023-11-30 10:40:57 -0600

    numpy practice

---
+++ numPyNotes.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+my_list = [1,2,3]
+
+arr = np.array(my_list)
+
+print(arr)
+
+print(np.arange(0,10))
+
+print(np.zeros((2,3)))
+
+print(np.ones((3,3)))
+
+print(np.linspace(0, 5, 3))
+
+print(np.random.randint(1,100, 5))
